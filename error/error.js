c07bb6d975360998361b6b7427b413dc076c5902
“NinjaAadi” <aadityapal.info@gmail.com>
2022-02-20 11:29:13 +0530

    Date and time functionality added

--- error/error.js
+++ error/error.js
@@ -1,7 +1,8 @@
 const logger = require("../logger/logger");
 //Custom module for handling errors
 const sendError = async (res, next, err, consoleMsg, responseMsg) => {
-  logger.error(new Error(consoleMsg, err));
+  logger.error(new Error(err));
+  logger.error(consoleMsg);
   return res.status(400).json({
     success: false,
     messege: responseMsg,
---
+++ routes/time/time.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Bring the controller functions
+const {
+  addTime,
+  getAllTime,
+  updateTime,
+  deleteTime,
+} = require("../../controllers/time/time");
+//Routes
+router.route("/addTime").post(addTime);
+router.route("/getAllTime").get(getAllTime);
+router.route("/updateTime").post(updateTime);
+router.route("/deleteTime").delete(deleteTime);
+
+module.exports = router;
